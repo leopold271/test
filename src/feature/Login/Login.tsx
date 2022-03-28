99efb68db4b58f0602bb97b2771f0872fc3ecd38
leopold271 <lenya.m1998@gmail.com>
2022-03-28 14:24:37 +0300

    second commit

--- src/feature/Login/Login.tsx
+++ src/feature/Login/Login.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { FormLayout, Form, TextField, Button } from "@shopify/polaris";
+import React, { useState } from "react";
+import { FormLayout, Form, TextField, Button, Icon } from "@shopify/polaris";
+import { ViewMinor } from '@shopify/polaris-icons';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import styles from './login.module.scss'
 
 export const Login = () => {
+
+  const [error, setError] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
   const loginSchema = yup.object().shape({
     password: yup
@@ -23,38 +27,55 @@ export const Login = () => {
     onSubmit: () => alert('submit')
   })
 
+  const handleChange = (value: string, id: string) => {
+    if (error) setError('');
+    formik.handleChange({ target: { id, value } });
+  };
+
+  const handleVisibilityIconClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsVisible(!isVisible);
+    e.stopPropagation();
+  }
+
   return (
-    <div className={styles.form__wrapper}>
-      <p>Please sign in</p>
-      <Form onSubmit={() => alert('submit')}>
-        <FormLayout>
-          <TextField
-            value={formik.values.email}
-            type='email'
-            id='email'
-            placeholder="example@gmail.com"
-            label='d'
-            labelHidden
-            error={formik.touched.email && formik.errors.email}
-            onChange={formik.handleChange}
-            autoComplete='off'
-          />
-          <TextField
-            value={formik.values.password}
-            type='password'
-            id='password'
-            placeholder="enter your password"
-            label='password'
-            labelHidden
-            error={formik.touched.password && formik.errors.password}
-            onChange={formik.handleChange}
-            autoComplete='off'
-          />
-        </FormLayout>
-        <Button submit outline>
-          Sign in
-        </Button>
-      </Form>
+    <div className={styles.form__position}>
+      <div className={styles.form__wrapper}>
+        <p>Please sign in</p>
+        <Form onSubmit={() => alert('submit')}>
+          <FormLayout>
+            <TextField
+              value={formik.values.email}
+              type='email'
+              id='email'
+              placeholder="example@gmail.com"
+              label='d'
+              labelHidden
+              error={formik.touched.email && formik.errors.email}
+              onChange={handleChange}
+              autoComplete='off'
+            />
+            <TextField
+              value={formik.values.password}
+              type={isVisible ? "text" : "password"}
+              id='password'
+              placeholder="enter your password"
+              label='password'
+              labelHidden
+              error={formik.touched.password && formik.errors.password}
+              onChange={handleChange}
+              autoComplete='off'
+              suffix=
+              {<div className={styles.form__visibilityButton} onClick={handleVisibilityIconClick}>
+                <Icon source={ViewMinor} color="base" />
+              </div>
+              }
+            />
+          </FormLayout>
+          <Button submit outline>
+            Sign in
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }
